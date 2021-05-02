03a61558cdf143e7357db0bf81bddb1b1602c882
ekta12345 <ektakumbhare3@gmail.com>
2021-05-02 14:36:10 +0530

    Added:Ability to insert between the consequetive nodes

--- Program.cs
+++ Program.cs
@@ -14,9 +14,15 @@ namespace DataStructure
             Console.WriteLine("Welcome to data structure");
             LinkedList list = new LinkedList();
 
+            //list.InsertFront(70);
+            //list.InsertFront(30);
+            //list.InsertFront(56);
             list.InsertFront(70);
-            list.InsertFront(30);
             list.InsertFront(56);
+
+            list.InsertAtParticularPosition(2, 30);
+            list.Display();
+            Console.ReadLine();
 
             list.Display();
             Console.ReadLine();
